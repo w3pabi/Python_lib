4fa98e7357cceb9ad12c372f137af14638d2d4fd
SungChangIm <pabi3564@gmail.com>
2014-12-11 00:15:06 +0900

    Text
    template

---
+++ Text/string_safe_substitute.py
@@ -0,0 +1,15 @@
+#String Template safe_substitute()
+
+
+import string
+
+values = { 'var' : 'foo'}
+
+t = string.Template("$var is here but $missing is not provided")
+
+try:
+    print 'substitute()     :', t.substitute(values)
+except KeyError, err:
+    print 'ERROR:', str(err)
+
+print 'safe_substitute():', t.safe_substitute(values)
